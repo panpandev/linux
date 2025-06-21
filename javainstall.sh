03b60e7d2d30bc113f315b35f58a0f373a3da979
panpandev <victorvazsilveira@gmail.com>
2025-06-20 21:36:18 -0300

    Update javainstall.sh

--- javainstall.sh
+++ javainstall.sh
@@ -1,1 +1,6 @@
-apt install openjdk-8-jre-headless -y && apt install openjdk-11-jre-headless -y && apt install apt install openjdk-17-jre-headless -y && apt install openjdk-21-jre-headless -y
+apt install openjdk-8-jdk-headless -y 
+apt install openjdk-11-jdk-headless -y 
+apt install openjdk-17-jdk-headless -y 
+apt install openjdk-18-jdk-headless -y 
+apt install openjdk-19-jre-headless -y 
+apt install openjdk-21-jdk-headless -y
